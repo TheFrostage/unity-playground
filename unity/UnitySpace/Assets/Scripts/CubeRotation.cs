793545570a6a9fb201ba841cbe12c2b81ff29e70
Nikita Perko <Perko.nikita@gmail.com>
2021-03-23 20:16:52 +0300

    added receiveing messages from RN

--- unity/UnitySpace/Assets/Scripts/CubeRotation.cs
+++ unity/UnitySpace/Assets/Scripts/CubeRotation.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace UnitySpace
@@ -7,9 +6,57 @@ namespace UnitySpace
     {
         [SerializeField]
         private float speed = 1;
+
+        private Vector3 _rotationVector = Vector3.right;
+
+        private void Start()
+        {
+            UnityMessageManager.Instance.OnMessage += OnMessage;
+        }
+
+        private void OnMessage(string message)
+        {
+            switch (message)
+            {
+                case "right":
+                    _rotationVector = Vector3.right;
+                    UnityMessageManager.Instance.SendMessageToRN("Chanded rotation to right");
+
+                    break;
+                case "up":
+                    _rotationVector = Vector3.up;
+
+                    UnityMessageManager.Instance.SendMessageToRN("Chanded rotation to up");
+
+                    break;
+                case "forward":
+                    _rotationVector = Vector3.forward;
+
+                    UnityMessageManager.Instance.SendMessageToRN("Chanded rotation to forward");
+
+                    break;
+                default:
+                    Debug.Log($"Incorrect message : {message}");
+
+                    break;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            UnityMessageManager.Instance.OnMessage -= OnMessage;
+        }
+
         private void Update()
         {
-            transform.rotation *= Quaternion.Euler(0,Time.deltaTime * speed,0);
+            var rotation = _rotationVector * Time.deltaTime * speed;
+            transform.rotation *= Quaternion.Euler(rotation.x, rotation.y, rotation.z);
+
+            if (Input.touchCount > 0)
+            {
+                UnityMessageManager.Instance.SendMessageToRN("Touches count = " + Input.touchCount);
+                Debug.Log("Detected touch");
+            }
         }
     }
 }
