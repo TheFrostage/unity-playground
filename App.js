866e5ecf4cd727841393ff60c84e02b992424b4d
ilya.semukha <i.semuha@freshlimesoft.com>
2021-03-23 14:39:28 +0300

    Added initial Unity intergation

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import UnityView from "@asmadsen/react-native-unity-view";
+import { UnityModule } from "@asmadsen/react-native-unity-view";
 
 export default function App() {
+  useEffect(() => {
+    const checkUnity = async () => {
+      const isUnityReady = await UnityModule.isReady();
+      console.log('is Unity ready?', isUnityReady);
+    };
+
+    checkUnity();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <UnityView style={styles.unity} />
+      <Text style={styles.welcomeText}>
+        Welcome to React Native!
+      </Text>
     </View>
   );
 }
@@ -14,8 +26,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'grey',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  unity: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+  },
+  welcomeText: {
+    fontSize: 14,
+    color: 'green',
   },
 });
